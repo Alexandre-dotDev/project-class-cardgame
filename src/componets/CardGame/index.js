5eaad83be6e3ceac98c29a9ce069a6231aeb0b85
Sonafa <alexandre.centro@yahoo.com.br>
2023-06-22 12:45:26 -0300

    primeiro comit

---
+++ src/componets/CardGame/index.js
@@ -0,0 +1,9 @@
+import './style.css';
+const CardGame = () =>{
+    return `
+    <article class="card-game"> 
+      <img src="src/img/alura-pixel .png" alt="Logo da Alura">                        
+    </article>
+    `;
+}
+export default CardGame;
